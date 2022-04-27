0604c57d94d905f63f7f56d8554ffba60704ebf3
susolr <jlopezpeque@gmail.com>
2022-04-27 18:41:48 +0200

    Barriga Conejo

--- juego/Conejo.js
+++ juego/Conejo.js
@@ -61,13 +61,12 @@ class Conejo extends THREE.Object3D {
       var pataGeom = new THREE.BoxGeometry(0.5, 2, 0.5);
       var pataI = new THREE.Mesh(pataGeom, mat);
       pataI.position.z = 1.25;
-      pataI.position.y = 1;
+      pataI.position.y = 0.875;
       pataI.position.x = 0.75;
 
-      var pataGeom = new THREE.BoxGeometry(0.5, 2, 0.5);
       var pataD = new THREE.Mesh(pataGeom, mat);
       pataD.position.z = 1.25;
-      pataD.position.y = 1;
+      pataD.position.y = 0.875;
       pataD.position.x = -0.75;
 
       var csg = new CSG();
