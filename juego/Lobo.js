486d19483b24cee2066f8fb68df94ce70299a053
susolr <jlopezpeque@gmail.com>
2022-04-28 00:20:34 +0200

    Lobo terminado

--- juego/Lobo.js
+++ juego/Lobo.js
@@ -10,29 +10,104 @@ class Lobo extends THREE.Object3D {
     this.createGUI(gui,titleGui);
     
     this.lobo = this.createLobo();
-    this.lobo.position.y = 1;
+    this.lobo.position.y = 1.5;
     this.add(this.lobo);
     
   }
 
   createLobo(){
       var mat = new THREE.MeshPhongMaterial({color: 0xFFFFFF});
+      var mat2 = new THREE.MeshPhongMaterial({color: 0x000000});
       var centralGeom = new THREE.BoxGeometry(1.5, 1.5, 4.5);
       var central = new THREE.Mesh(centralGeom, mat);
-      var centralGordaGeom = new THREE.BoxGeometry(2, 1.75, 1.5);
-      var centralGorda = new THREE.Mesh(centralGordaGeom, mat);
-      centralGorda.position.z = 0.75;
+      var cuelloGeom = new THREE.BoxGeometry(2, 1.75, 1.5);
+      var cuello = new THREE.Mesh(cuelloGeom, mat);
+      cuello.position.z = 0.75;
 
       var colaGeom = new THREE.BoxGeometry(0.5, 0.5, 1.75);
       var cola = new THREE.Mesh(colaGeom, mat);
       cola.position.z = -2.5;
       cola.position.y = 0.5;
+
+      var hocicoSGeom = new THREE.BoxGeometry(0.75, 0.5, 1);
+      var hocicoS = new THREE.Mesh(hocicoSGeom, mat);
+      hocicoS.position.y = 0.125;
+      var hocicoIGeom = new THREE.BoxGeometry(0.75, 0.25, 1);
+      var hocicoI = new THREE.Mesh(hocicoIGeom, mat2);
+      hocicoI.position.y = -0.25;
+
+      var hocico = new THREE.Object3D();
+      hocico.add(hocicoS);
+      hocico.add(hocicoI);
+      hocico.position.z = 2.625;
+      hocico.position.y = -0.25;
+
+      var orejaGeom = new THREE.BoxGeometry(0.5, 0.5, 0.25);
+
+      var orejaI = new THREE.Mesh(orejaGeom, mat);
+      orejaI.position.z = 1.5;
+      orejaI.position.y = 1;
+      orejaI.position.x = 0.5;
+
+      var orejaD = new THREE.Mesh(orejaGeom, mat);
+      orejaD.position.z = 1.5;
+      orejaD.position.y = 1;
+      orejaD.position.x = -0.5;
+
+      var cuboGeom = new THREE.BoxGeometry(0.25, 0.25, 0.25);
+      
+      var nariz = new THREE.Mesh(cuboGeom, mat2);
+      nariz.position.z = 3.0625;
+      nariz.position.y = 0.0625;
+
+      var ojoI = new THREE.Mesh(cuboGeom, mat2);
+      ojoI.position.x = 0.375;
+      ojoI.position.z = 2.25;
+      ojoI.position.y = 0.25;
+
+      var ojoD = new THREE.Mesh(cuboGeom, mat2);
+      ojoD.position.x = -0.375;
+      ojoD.position.z = 2.25;
+      ojoD.position.y = 0.25;
+
       var cuerpo = new THREE.Object3D();
       cuerpo.add(central);
-      cuerpo.add(centralGorda);
+      cuerpo.add(cuello);
       cuerpo.add(cola);
+      cuerpo.add(hocico);
+      cuerpo.add(orejaI);
+      cuerpo.add(orejaD);
+      cuerpo.add(nariz);
+      cuerpo.add(ojoI);
+      cuerpo.add(ojoD);
 
-      return cuerpo;
+      cuerpo.position.y = 1.5;
+
+      var pataGeom = new THREE.BoxGeometry(0.5, 1.75, 0.5);
+      var pataDI = new THREE.Mesh(pataGeom, mat);
+      pataDI.position.x = 0.5;
+      pataDI.position.z = 0.75;
+
+      var pataDD = new THREE.Mesh(pataGeom, mat);
+      pataDD.position.x = -0.5;
+      pataDD.position.z = 0.75;
+
+      var pataTI = new THREE.Mesh(pataGeom, mat);
+      pataTI.position.x = 0.5;
+      pataTI.position.z = -1.5;
+
+      var pataTD = new THREE.Mesh(pataGeom, mat);
+      pataTD.position.x = -0.5;
+      pataTD.position.z = -1.5;
+
+      var completo = new THREE.Object3D();
+      completo.add(cuerpo);
+      completo.add(pataDI);
+      completo.add(pataDD);
+      completo.add(pataTI);
+      completo.add(pataTD);
+
+      return completo;
 
   }
   
