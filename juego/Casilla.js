56d724dd16073189d82b0f7963051b4b038e7a97
susolr <jlopezpeque@gmail.com>
2022-05-04 17:25:06 +0200

    Clase casilla

---
+++ juego/Casilla.js
@@ -0,0 +1,41 @@
+import * as THREE from '../libs/three.module.js'
+import { CSG } from '../libs/CSG-v2.js'
+ 
+class Casilla extends THREE.Object3D {
+  constructor(tipo) {
+    super();
+    
+    // Se crea la parte de la interfaz que corresponde a la caja
+    // Se crea primero porque otros métodos usan las variables que se definen para la interfaz
+    //this.createGUI(gui,titleGui);
+    
+    this.model = this.createCasilla(tipo);
+    this.add(this.model);
+
+    this.casillasAccesibles = [];
+    
+  }
+
+  createCasilla(tipo){
+    var casilla;
+    var boxGeom = new THREE.BoxBufferGeometry (5, 1, 5);
+    var boxMat = new THREE.MeshPhongMaterial({color: 0x6d36a3});
+    var cilMat = new THREE.MeshPhongMaterial({color: 0x6fb53a});
+    var cilGeom = new THREE.CylinderGeometry( 3, 3, 1, 8);
+
+    if (tipo === 0){
+        casilla = new THREE.Mesh(cilGeom, cilMat);
+    }
+    else {
+        casilla = new THREE.Mesh(boxGeom, boxMat); 
+    }
+
+    return casilla;
+  }
+  
+  update () {
+
+  }
+}
+
+export { Casilla };
--- juego/Tablero.js
+++ juego/Tablero.js
@@ -1,4 +1,5 @@
 import * as THREE from '../libs/three.module.js'
+import { Casilla } from './Casilla.js';
  
 class Tablero extends THREE.Object3D {
   constructor() {
@@ -26,66 +27,66 @@ class Tablero extends THREE.Object3D {
     this.casillasIndexadas = new Array(11);
     // Y añadirlo como hijo del Object3D (el this)
 
-    var model = new THREE.Mesh(cilGeom, cilMat);
+    var model = new Casilla(0);
     model.rotateY(THREE.MathUtils.degToRad(22.5));
     model.position.x = -20;
     this.add(model);
     this.casillasIndexadas[0] = model;
 
-    model = new THREE.Mesh(cilGeom, cilMat);
+    model = new Casilla(0);
     model.rotateY(THREE.MathUtils.degToRad(22.5));
     model.position.x = -10;
     model.position.z = -10;
     this.add(model);
     this.casillasIndexadas[1] = model;
 
-    model = new THREE.Mesh(boxGeom, boxMat);
+    model = new Casilla(1);
     model.position.x = -10;
     this.add(model);
     this.casillasIndexadas[2] = model;
 
-    model = new THREE.Mesh(cilGeom, cilMat);
+    model = new Casilla(0);
     model.rotateY(THREE.MathUtils.degToRad(22.5));
     model.position.x = -10;
     model.position.z = 10;
     this.add(model);
     this.casillasIndexadas[3] = model;
 
-    model = new THREE.Mesh(boxGeom, boxMat);
+    model = new Casilla(1);
     model.position.z = -10;
     this.add(model);
     this.casillasIndexadas[4] = model;
 
-    model = new THREE.Mesh(cilGeom, cilMat);
+    model = new Casilla(0);
     model.rotateY(THREE.MathUtils.degToRad(22.5));
     this.add(model);
     this.casillasIndexadas[5] = model;
 
-    model = new THREE.Mesh(boxGeom, boxMat);
+    model = new Casilla(1);
     model.position.z = 10;
     this.add(model);
     this.casillasIndexadas[6] = model;
 
-    model = new THREE.Mesh(cilGeom, cilMat);
+    model = new Casilla(0);
     model.rotateY(THREE.MathUtils.degToRad(22.5));
     model.position.x = 10;
     model.position.z = -10;
     this.add(model);
     this.casillasIndexadas[7] = model;
 
-    model = new THREE.Mesh(boxGeom, boxMat);
+    model = new Casilla(1);
     model.position.x = 10;
     this.add(model);
     this.casillasIndexadas[8] = model;
 
-    model = new THREE.Mesh(cilGeom, cilMat);
+    model = new Casilla(0);
     model.rotateY(THREE.MathUtils.degToRad(22.5));
     model.position.x = 10;
     model.position.z = 10;
     this.add(model);
     this.casillasIndexadas[9] = model;
 
-    model = new THREE.Mesh(cilGeom, cilMat);
+    model = new Casilla(0);
     model.position.x = 20;
     this.add(model);
     this.casillasIndexadas[10] = model;
