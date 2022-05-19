e66f5724e5001f5cb3aa4a255204f6ccf77d58ca
Esmeralda <esmeraldajm99@gmail.com>
2022-05-19 18:23:46 +0200

    textura

--- juego/Lobo.js
+++ juego/Lobo.js
@@ -61,7 +61,7 @@ class Lobo extends THREE.Object3D {
   }
 
   createLobo(){
-      var texture = new THREE.TextureLoader().load('../imgs/pelaje.jpg');
+      var texture = new THREE.TextureLoader().load('../imgs/pelaje2.jpg');
       var mat = new THREE.MeshPhongMaterial ({map: texture});
       //var mat = new THREE.MeshPhongMaterial({color: 0xe6e6e6}); // Gris
       var matn = new THREE.MeshPhongMaterial({color: 0x000000}); // Negro
