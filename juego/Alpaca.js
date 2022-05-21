356cc1fa1b0e4db2e989287c351dbe4f65d3f5cc
Esmeralda <esmeraldajm99@gmail.com>
2022-05-21 03:27:32 +0200

    Alpaca

--- juego/Alpaca.js
+++ juego/Alpaca.js
@@ -4,7 +4,7 @@ import * as TWEEN from '../libs/tween.esm.js'
 import { FirstPersonControls } from '../libs/FirstPersonControls.js'
 import { OrbitControls } from '../libs/OrbitControls.js'
  
-class Lobo extends THREE.Object3D {
+class Alpaca extends THREE.Object3D {
   constructor(renderer) {
     super();
     
@@ -12,12 +12,10 @@ class Lobo extends THREE.Object3D {
     // Se crea primero porque otros métodos usan las variables que se definen para la interfaz
     //this.createGUI(gui,titleGui);
     this.createCamera(renderer);
-    this.model = this.createLobo();
+    this.model = this.createAlpaca();
     this.model.add(this.camera);
-    this.model.position.y = 1.35;
+    this.model.position.y = 1.5; //2 si quitamos escalado
     this.add(this.model);
-    
-    
 
     this.mov_d = 0;
     this.mov_i = 0;
@@ -31,8 +29,8 @@ class Lobo extends THREE.Object3D {
   }
 
   createLight(){
-    var light = new THREE.SpotLight(0xfcfcfc, 3, 6, Math.PI/4);
-    light.position.set(0, 6, 0);
+    var light = new THREE.SpotLight(0xfcfcfc, 2, 10, Math.PI/4);
+    light.position.set(0, 10, 0);
     light.target = this.model;
     return light;
   }
@@ -42,7 +40,7 @@ class Lobo extends THREE.Object3D {
     this.camera = new THREE.PerspectiveCamera ( 75 , window.innerWidth/window.innerHeight, 0.1 , 1000) ;
     this.camera.position.set(2.25,3,0);
     this.camera.lookAt( 0 , 0 , 0 ) ;
-    // Se crea e l c o n t r o l de v u e l o
+    // Se crea el control de vuelo
     this.fpControls = new FirstPersonControls ( this.camera , renderer.domElement ) ;
     //this.fpControls = new OrbitControls(this.camera, renderer.domElement);
     this.fpControls.movementSpeed = 25 ;
@@ -60,170 +58,187 @@ class Lobo extends THREE.Object3D {
     this.light.visible = false;
   }
 
-  createLobo(){
-      var texture = new THREE.TextureLoader().load('../imgs/pelaje2.jpg');
+  createAlpaca(){
+      var texture = new THREE.TextureLoader().load('../imgs/pelajealpaca.jpg');
       var mat = new THREE.MeshPhongMaterial ({map: texture});
       //var mat = new THREE.MeshPhongMaterial({color: 0xe6e6e6}); // Gris
       var matn = new THREE.MeshPhongMaterial({color: 0x000000}); // Negro
-      var matb = new THREE.MeshPhongMaterial({color: 0xFFFFFF}); // Blanco
+      var matm = new THREE.MeshPhongMaterial({color: 0x6c3b2a}); // Marrón
 
-      // Parte central (cabeza y lomo)
-      var centralGeom = new THREE.BoxGeometry(1.5, 1.5, 4.5);
-      var central = new THREE.Mesh(centralGeom, mat);
-      central.userData = this;
+      var hocicoGeom = new THREE.BoxGeometry(1, 1, 1); // Hocico
+      var hocico = new THREE.Mesh(hocicoGeom, mat);
+      hocico.userData = this;
 
-      // Cuello
-      var cuelloGeom = new THREE.BoxGeometry(2, 1.75, 1.5);
-      var cuello = new THREE.Mesh(cuelloGeom, mat);
-      cuello.userData = this;
-      cuello.position.z = 0.75;
+      var narizGeom = new THREE.BoxGeometry(0.25, 0.25, 0.25); // Nariz izquierda
+      var narizI = new THREE.Mesh(narizGeom, matn);
+      narizI.userData = this;
+      narizI.position.x = 0.325;
 
-      // Cola
-      var colaGeom = new THREE.BoxGeometry(0.5, 0.5, 1.75);
-      var cola = new THREE.Mesh(colaGeom, mat);
-      cola.userData = this;
-      cola.position.z = -2.5;
-      cola.position.y = 0.48;
+      var narizD = new THREE.Mesh(narizGeom, matn); // Nariz derecha
+      narizD.userData = this;
+      narizD.position.x = -0.325;
 
-      // Hocico parte superior
-      var hocicoSGeom = new THREE.BoxGeometry(0.75, 0.5, 1);
-      var hocicoS = new THREE.Mesh(hocicoSGeom, mat);
-      hocicoS.userData = this;
-      hocicoS.position.y = 0.125;
+      var nariz = new THREE.Object3D(); // Nariz completa (fosa izquierda y derecha)
+      nariz.add(narizI);
+      nariz.add(narizD);
+      nariz.position.y = 0.125;
+      nariz.position.z = 0.38;
 
-      // Hocico parte inferior
-      var hocicoIGeom = new THREE.BoxGeometry(0.75, 0.25, 1);
-      var hocicoI = new THREE.Mesh(hocicoIGeom, matn);
-      hocicoI.userData = this;
-      hocicoI.position.y = -0.25;
+      var bocaSGeom = new THREE.BoxGeometry(0.4, 0.25, 0.25); // Boca superior
+      var bocaS = new THREE.Mesh(bocaSGeom, matm);
+      bocaS.userData = this;
 
-      // Nariz
-      var cuboGeom = new THREE.BoxGeometry(0.25, 0.25, 0.25);
-      var nariz = new THREE.Mesh(cuboGeom, matn);
-      nariz.userData= this;
-      nariz.position.z = 0.4375;
-      nariz.position.y = 0.3125;
+      var bocaIGeom = new THREE.BoxGeometry(1.02, 0.25, 0.25); // Boca inferior
+      var bocaI = new THREE.Mesh(bocaIGeom, matm);
+      bocaI.userData = this;
+      bocaI.position.y = -0.25;
 
-      // Hocico completo (parte superior e inferior y nariz)
-      var hocico = new THREE.Object3D();
-      hocico.add(hocicoS);
-      hocico.add(hocicoI);
-      hocico.add(nariz);
-      hocico.position.z = 2.625;
-      hocico.position.y = -0.25;
+      var boca = new THREE.Object3D(); // Boca completa (parte superior e inferior)
+      boca.add(bocaS);
+      boca.add(bocaI);
+      boca.position.y = -0.13;
+      boca.position.z = 0.38;
+      
+      var hocicocompleto = new THREE.Object3D(); // Hocico completo (hocico, nariz y boca)
+      hocicocompleto.add(hocico);
+      hocicocompleto.add(nariz);
+      hocicocompleto.add(boca);
+      hocicocompleto.position.y = 1.25;
+      hocicocompleto.position.z = 1.25;
 
-      // Oreja izquierda
-      var orejaGeom = new THREE.BoxGeometry(0.5, 0.5, 0.25);
-      var orejaI = new THREE.Mesh(orejaGeom, mat);
-      orejaI.userData = this;
-      orejaI.position.x = 0.5;
+      var cabezaGeom = new THREE.BoxGeometry(2, 4.5, 1.5); // Cabeza
+      var cabeza = new THREE.Mesh(cabezaGeom, mat);
+      cabeza.userData = this;
 
-      // Oreja derecha
-      var orejaD = new THREE.Mesh(orejaGeom, mat);
-      orejaD.userData = this;
-      orejaD.position.x = -0.5;
+      var orejaEGeom = new THREE.BoxGeometry(0.75, 0.75, 0.5); // Oreja izquierda parte exterior
+      var orejaIE = new THREE.Mesh(orejaEGeom, mat);
+      orejaIE.userData = this;
 
-      // Orejas (izquierda y derecha)
-      var orejas = new THREE.Object3D();
+      var orejaIGeom = new THREE.BoxGeometry(0.25, 0.5, 0.25); // Oreja izquierda parte interior
+      var orejaIM = new THREE.Mesh(orejaIGeom, matm);
+      orejaIM.userData = this;
+      orejaIM.position.y = -0.125;
+      orejaIM.position.z = 0.15;
+
+      var orejaI = new THREE.Object3D(); // Oreja izquierda completa (parte exterior e interior)
+      orejaI.add(orejaIE);
+      orejaI.add(orejaIM);
+      orejaI.position.x = 0.6;
+
+      var orejaDE = new THREE.Mesh(orejaEGeom, mat); // Oreja derecha parte exterior
+      orejaDE.userData = this;
+
+      var orejaDM = new THREE.Mesh(orejaIGeom, matm); // Oreja derecha parte interior
+      orejaDM.userData = this;
+      orejaDM.position.y = -0.125;
+      orejaDM.position.z = 0.15;
+
+      var orejaD = new THREE.Object3D(); // Oreja derecha completa (parte exterior e interior)
+      orejaD.add(orejaDE);
+      orejaD.add(orejaDM);
+      orejaD.position.x = -0.6;
+
+      var orejas = new THREE.Object3D(); // Orejas completas (izquierda y derecha)
       orejas.add(orejaI);
       orejas.add(orejaD);
-      orejas.position.z = 1.75;
-      orejas.position.y = 1;
+      orejas.position.y = 2.625; 
 
-      // Pupila ojo izquierdo
-      var ojonI = new THREE.Mesh(cuboGeom, matn);
-      ojonI.userData = this;
-      // Esclerótica ojo izquierdo
-      var ojobI = new THREE.Mesh(cuboGeom, matb);
-      ojobI.userData = this;
-      ojobI.position.x = 0.25;
+      var ojoMGeom = new THREE.BoxGeometry(0.5, 0.25, 0.25); // Ojo izquierdo párpado
+      var ojoIM = new THREE.Mesh(ojoMGeom, matm);
+      ojoIM.userData = this;
+      ojoIM.position.y = 0.25;
 
-      // Ojo izquierdo (pupila y esclerótica)
-      var ojoI = new THREE.Object3D();
-      ojoI.add(ojonI);
-      ojoI.add(ojobI);
-      ojoI.position.x = 0.35;
+      var ojoNGeom = new THREE.BoxGeometry(0.5, 0.25, 0.25); // Ojo izquierdo pupila
+      var ojoIN = new THREE.Mesh(ojoNGeom, matn);
+      ojoIN.userData = this;
 
-      // Pupila ojo derecho
-      var ojonD = new THREE.Mesh(cuboGeom, matn);
-      // Esclerótica ojo derecho
-      var ojobD = new THREE.Mesh(cuboGeom, matb);
-      ojobD.position.x = -0.25;
+      var ojoI = new THREE.Object3D(); // Ojo izquierdo completo (párpado y pupila)
+      ojoI.add(ojoIM);
+      ojoI.add(ojoIN);
+      ojoI.position.x = 0.5;
 
-      // Ojo derecho (pupila y esclerótica)
-      var ojoD = new THREE.Object3D();
-      ojoD.add(ojonD);
-      ojoD.add(ojobD);
-      ojoD.position.x = -0.35;
+      var ojoDM = new THREE.Mesh(ojoMGeom, matm); // Ojo derecho párpado
+      ojoDM.userData = this;
+      ojoDM.position.y = 0.25;
 
-      // Ojos completos (izquierdo y derecho)
-      var ojos = new THREE.Object3D();
-      ojos.add(ojoD);
+      var ojoDN = new THREE.Mesh(ojoNGeom, matn); // Ojo derecho pupila
+      ojoDN.userData = this;
+
+      var ojoD = new THREE.Object3D(); // Ojo derecho completo (párpado y pupila)
+      ojoD.add(ojoDM);
+      ojoD.add(ojoDN);
+      ojoD.position.x = -0.5;
+
+      var ojos = new THREE.Object3D(); // Ojos completos (izquierdo y derecho)
       ojos.add(ojoI);
-      ojos.position.z = 2.25;
-      ojos.position.y = 0.25;
+      ojos.add(ojoD);
+      ojos.position.y = 1.625;
+      ojos.position.z = 0.75; 
 
-      // Cuerpo sin patas
-      var cuerpo = new THREE.Object3D();
-      cuerpo.add(central);
-      cuerpo.add(cuello);
-      cuerpo.add(cola);
-      cuerpo.add(hocico);
-      cuerpo.add(orejas);
-      cuerpo.add(ojos);
+      var cabezacompleta = new THREE.Object3D(); // Cabeza completa (hocico, cabeza, orejas y ojos)
+      cabezacompleta.add(hocicocompleto);
+      cabezacompleta.add(cabeza);
+      cabezacompleta.add(orejas);
+      cabezacompleta.add(ojos);
+      cabezacompleta.position.y = 1.5;
+      cabezacompleta.position.z = 2.375; 
 
-      cuerpo.position.y = 1.5;
+      var cuerpoGeom = new THREE.BoxGeometry(3, 2.5, 4.5); // Cuerpo
+      var cuerpo = new THREE.Mesh(cuerpoGeom, mat);
+      cuerpo.userData = this;
+
+      var cuerpocompleto = new THREE.Object3D(); // Cuerpo completo sin patas (cabeza y cuerpo)
+      cuerpocompleto.add(cabezacompleta);
+      cuerpocompleto.add(cuerpo);
+      cuerpocompleto.position.y = 2.5;
 
       // Pata delantera izquierda
-      var pataGeom = new THREE.BoxGeometry(0.5, 1.75, 0.5);
+      var pataGeom = new THREE.BoxGeometry(1, 3, 1);
       var auxpataDI = new THREE.Mesh(pataGeom, mat);
-      auxpataDI.position.x = 0.5;
-      auxpataDI.position.y = -0.625;
+      auxpataDI.position.x = 0.75;
+      auxpataDI.position.y = -1;
       this.pataDI = new THREE.Object3D();
       this.pataDI.add(auxpataDI);
-      this.pataDI.position.z = 0.75;
-      this.pataDI.position.y = 0.625;
+      this.pataDI.position.z = 1;
+      this.pataDI.position.y = 1;
       
-
       // Pata delantera derecha
       var auxpataDD = new THREE.Mesh(pataGeom, mat);
-      auxpataDD.position.x = -0.5;
-      auxpataDD.position.y = -0.625;
+      auxpataDD.position.x = -0.75;
+      auxpataDD.position.y = -1;
       this.pataDD = new THREE.Object3D();
       this.pataDD.add(auxpataDD);
-      this.pataDD.position.z = 0.75;
-      this.pataDD.position.y = 0.625;
+      this.pataDD.position.z = 1;
+      this.pataDD.position.y = 1;
 
       // Pata trasera izquierda
       var auxpataTI = new THREE.Mesh(pataGeom, mat);
-      auxpataTI.position.x = 0.48;
-      auxpataTI.position.y = -0.625;
+      auxpataTI.position.x = 0.75;
+      auxpataTI.position.y = -1;
       this.pataTI = new THREE.Object3D();
       this.pataTI.add(auxpataTI);
-      this.pataTI.position.z = -1.5;
-      this.pataTI.position.y = 0.625;
+      this.pataTI.position.z = -1.25;
+      this.pataTI.position.y = 1;
 
       // Pata trasera derecha
       var auxpataTD = new THREE.Mesh(pataGeom, mat);
-      auxpataTD.position.x = -0.48;
-      auxpataTD.position.y = -0.625;
+      auxpataTD.position.x = -0.75;
+      auxpataTD.position.y = -1;
       this.pataTD = new THREE.Object3D();
       this.pataTD.add(auxpataTD);
-      this.pataTD.position.y = 0.625;
-      this.pataTD.position.z = -1.5;
+      this.pataTD.position.z = -1.25;
+      this.pataTD.position.y = 1;
+      
 
-      // Cuerpo completo
       var completo = new THREE.Object3D();
-      completo.add(cuerpo);
+      completo.add(cuerpocompleto);
       completo.add(this.pataDI);
       completo.add(this.pataDD);
       completo.add(this.pataTI);
       completo.add(this.pataTD);
-
-      // Subimos el lobo la mitad de la altura de las patas para que esté en la base
-      completo.position.y = 0.875;
+      completo.position.y = 1.5; // Subimos la alpaca la mitad de la altura de las patas para que esté en la base
+      completo.scale.set(0.75,0.75,0.75);
       return completo;
-
   }
 
   controlAnimacion () {
@@ -273,6 +288,8 @@ class Lobo extends THREE.Object3D {
       this.animation.start();
   }
   
+  
+
   update () {
     var delta = this.clock.getDelta() ;
     this.fpControls.update(delta) ;
@@ -321,4 +338,4 @@ class Lobo extends THREE.Object3D {
   }
 }
 
-export { Lobo };
+export { Alpaca };
